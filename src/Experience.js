5ba7fca5d0db0f53de27ceb0faf85f2d74cc40a2
Seoyeon Kim <seoyeonkim815@gmail.com>
2022-12-18 18:06:06 +0900

    [R3F] Added Transform?Controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,6 +1,17 @@
+import { useRef } from "react";
+import { TransformControls, OrbitControls } from "@react-three/drei";
+
 export default function Experience() {
+  /**** Hooks  ****/
+
+  const cubeRef = useRef();
+
+  /**** Render  ****/
+
   return (
     <>
+      <OrbitControls makeDefault />
+
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
@@ -8,12 +19,11 @@ export default function Experience() {
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
-
-      <mesh position-x={2} scale={1.5}>
+      <mesh position-x={2} scale={1.5} ref={cubeRef}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
-
+      <TransformControls object={cubeRef} mode="translate" />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
