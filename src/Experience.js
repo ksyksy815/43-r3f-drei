bf4228ded8f155e1cb38d7f466e68448dfc57e2b
Seoyeon Kim <seoyeonkim815@gmail.com>
2022-12-18 19:42:32 +0900

    [R3F] Applied reflective material to the plane(floor)

--- src/Experience.js
+++ src/Experience.js
@@ -6,6 +6,7 @@ import {
   Html,
   Text,
   Float,
+  MeshReflectorMaterial,
 } from "@react-three/drei";
 
 export default function Experience() {
@@ -48,7 +49,13 @@ export default function Experience() {
       <TransformControls object={cubeRef} mode="translate" />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <MeshReflectorMaterial
+          resolution={500}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.75}
+          color="greenyellow"
+        />
       </mesh>
       <Float speed={5} floatIntensity={2}>
         <Text
